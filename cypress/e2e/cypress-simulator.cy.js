1e5cc2199ee440e5b062a58ca4b2ff031fafd125
Renan Dias <renan7k@gmail.com>
2025-04-22 17:55:59 -0300

    create test plan

---
+++ cypress/e2e/cypress-simulator.cy.js
@@ -0,0 +1,57 @@
+describe('template spec', () => {
+  it('success', () => {
+  })
+
+  it('error: invalid cypress command', () => {
+  })
+
+  it('warning', () => {
+  })
+
+  it('error: valid command without parentheses', () => {
+  })
+
+  it('help', () => {
+  })
+
+  it('maximize/minimize', () => {
+  })
+
+  it('logout', () => {
+  })
+
+  it('show and hide logout button', () => {
+  })
+
+  it('running ... state', () => {
+  })
+
+  it('Accept cookies', () => {
+  })
+
+  it('Declined cookies', () => {
+  })
+
+  it('Captcha button state', () => {
+  })
+
+  it('Captcha error', () => {
+  })
+
+  it('Run button - enabled/disabled state', () => {
+  })
+
+  
+  it('Reset text area on logout and login', () => {
+  })
+
+  it('Disabled run button on logout and login', () => {
+  })
+
+  it('Reset output on logout and login', () => {
+  })
+
+  it('No cooking banner on the login page', () => {
+  })
+
+})
