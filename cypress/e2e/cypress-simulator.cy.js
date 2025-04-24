2d72953be418a67228d6ac8468ddd8f690798bd5
Renan Dias <renan7k@gmail.com>
2025-04-24 17:54:23 -0300

    add test banner cookies

--- cypress/e2e/cypress-simulator.cy.js
+++ cypress/e2e/cypress-simulator.cy.js
@@ -119,27 +119,60 @@ describe("Cypress Simulator", () => {
       .and("contain", "Success:")
       .and("contain", "cy.visit('https://google.com') // Visited URL 'https://google.com'")
   })
+  it('checks the run button disabled and enabled states', () => {
+    cy.contains("button", "Run").should("be.disabled")
+    cy.get("textarea[placeholder='Write your Cypress code here...']").type("cy.log('Teste')")
 
-  it('Captcha button state', () => {
-  })
+    cy.contains("button", "Run").should("be.enabled")
 
-  it('Captcha error', () => {
-  })
+    cy.get("textarea[placeholder='Write your Cypress code here...']").clear()
 
-  it('Run button - enabled/disabled state', () => {
+    cy.contains("button", "Run").should("be.disabled")
   })
 
   
-  it('Reset text area on logout and login', () => {
+  it('clears the code input when logging off then logging in again', () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']").type("cy.log('Teste')")
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").click()
+    cy.contains("button", "Login").click()
+
+    cy.get("textarea[placeholder='Write your Cypress code here...']").should("have.value", "")
+
   })
 
-  it('Disabled run button on logout and login', () => {
+  it('disables the run button when logging off then logging in again', () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']").type("cy.log('Teste')")
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").click()
+    cy.contains("button", "Login").click()
+
+    cy.contains("button", "Run").should("be.disabled")
   })
 
-  it('Reset output on logout and login', () => {
+  it('clears the code output when logging off then logging in again', () => {
+    cy.get("textarea[placeholder='Write your Cypress code here...']").type("cy.log('Teste')")
+    cy.contains("button", "Run").click()
+
+    cy.get("#outputArea", {timeout: 6000})
+      .should("be.visible")
+      .and("contain", "Success:")
+      .and("contain", "cy.log('Teste') // Logged message 'Teste'")
+
+
+    cy.get("#sandwich-menu").click()
+    cy.contains("button", "Logout").click()
+    cy.contains("button", "Login").click()
+
+    cy.get("#outputArea").should("not.contain", "cy.log('Teste') // Logged message 'Teste'")
   })
 
-  it('No cooking banner on the login page', () => {
+  it.only("doesn't show the cookie consent banner on the login page", () => {
+    cy.clearAllLocalStorage()
+    cy.reload()
+
+    cy.contains("button", "Login").should("be.visible")
+    cy.get("#cookieConsent").should("not.be.visible")
   })
 
 })
@@ -176,5 +209,37 @@ describe("Cypress Simulator - Cookies consent", () => {
     cy.window()
       .its("localStorage.cookieConsent")
       .should("be.equal", "declined")
+  })
+
+
+})
+
+describe("Cypress Simulator - Captcha", () => {
+  beforeEach(() => {
+    cy.visit("./src/index.html")
+    cy.contains("button", "Login").click()
+  })
+  it("disables the captcha verify button when no answer is provided or it's cleared", () => {
+    cy.contains("button", "Verify").should("be.disabled")
+
+    cy.get("input[placeholder='Enter your answer']").type("1")
+
+    cy.contains("button", "Verify").should("be.enabled")
+
+    cy.get("input[placeholder='Enter your answer']").clear()
+
+    cy.contains("button", "Verify").should("be.disabled")
+  })
+
+  it("shows an error on a wrong captcha answer and goes back to its initial state", () => {
+    cy.get("input[placeholder='Enter your answer']").type("1000")
+
+    cy.contains("button", "Verify").click()
+    
+    cy.contains(".error", "Incorrect answer, please try again.").should("be.visible")
+
+    cy.get("input[placeholder='Enter your answer']").should("have.value","")
+    cy.contains("button", "Verify").should("be.disabled")
+    
   })
 })
